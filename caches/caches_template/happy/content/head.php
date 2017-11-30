31ec70a636136f161528ca52f7871be9a4986e03
zhanglizhao <601144080@qq.com>
2017-11-30 13:50:41 +0800

    add .

--- caches/caches_template/happy/content/head.php
+++ caches/caches_template/happy/content/head.php
@@ -8,16 +8,17 @@
     <link rel="stylesheet" href="<?php echo CSS_PATH;?>happy/base.css" type="text/css"/>
     <link rel="stylesheet" href="<?php echo CSS_PATH;?>happy/animate.min.css" type="text/css"/>
     <link rel="stylesheet" href="<?php echo CSS_PATH;?>happy/index.css" type="text/css"/>
+    <script src="<?php echo JS_PATH;?>happy/jquery/jquery.js"></script>
 </head>
 <body>
 <div class="happy">
 <div class="main">
-    <div class="main-nav">
-        <div class="main-nav-le">
-            <img src="<?php echo IMG_PATH;?>image/shouye/le.png" alt=""/>
-        </div>
+    <div class="main-nav">        
         <!--导航栏-->
         <div class="main-nav-ion">
+            <div class="main-nav-le">
+                <img src="<?php echo IMG_PATH;?>image/shouye/le.png" alt="" />
+            </div>
             <?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=172d3e742399e5ace943301e16f256b0&action=category&catid=0&num=10&siteid=%24siteid&order=listorder+ASC\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'category')) {$data = $content_tag->category(array('catid'=>'0','siteid'=>$siteid,'order'=>'listorder ASC','limit'=>'10',));}?>
             <ul class="clearfix">
                 <li class="ion-t"><a  href="<?php echo siteurl($siteid);?>">网站首页</a></li>
--- caches/caches_template/happy/content/list.php
+++ caches/caches_template/happy/content/list.php
@@ -1,15 +1,17 @@
 <?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><?php include template("content","head"); ?>
+<div class="content-box">
 <?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=7a77e246016a6e448ceab1565dff0a17&action=lists&catid=%24catid&order=id+DESC&num=5&page=%24page\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'lists')) {$pagesize = 5;$page = intval($page) ? intval($page) : 1;if($page<=0){$page=1;}$offset = ($page - 1) * $pagesize;$content_total = $content_tag->count(array('catid'=>$catid,'order'=>'id DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));$pages = pages($content_total, $page, $pagesize, $urlrule);$data = $content_tag->lists(array('catid'=>$catid,'order'=>'id DESC','limit'=>$offset.",".$pagesize,'action'=>'lists',));}?>
 <!-- <div class="title"><?php echo $CATEGORYS[$catid]['catname'];?></div>
  <div class="list-box">-->
+<div class="title"><?php echo $CATEGORYS[$catid]['catname'];?></div>
 <ul class="clearfix">
     <?php $n=1; if(is_array($data)) foreach($data AS $key => $val) { ?>
     <li class="MyBox">
         <a class="img" href="<?php echo $val['url'];?>">
             <img src="<?php if($val[thumb]) { ?><?php echo thumb($val[thumb],220,170);?><?php } else { ?><?php echo IMG_PATH;?>qk_img/noImg.jpg<?php } ?>" alt="<?php echo $val['title'];?>" title="<?php echo $val['title'];?>"/>
         </a>
-        <a class="body" href="<?php echo $val['url'];?>">
-            <div class="head"><?php echo $val['title'];?></div><br/>
+        <a class="clearfix" href="<?php echo $val['url'];?>">
+            <div class="head"><?php echo $val['title'];?></div>
             <div class="info"><?php echo $val['description'];?></div>
         </a>
     </li>
@@ -20,4 +22,5 @@
     <?php echo $pages;?>
 </div>
 <?php if(defined('IN_ADMIN') && !defined('HTML')) {echo '</div>';}?>
+</div>
 <?php include template("content","foot"); ?>
